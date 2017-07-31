f5188d2dd94b6c036c4a8d17004bedcee04cc984
Andrey <fyling@i.ua>
2017-07-31 16:04:31 +0300

    HW03

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
+
 
 CREATE SEQUENCE global_seq START 100000;
 
@@ -22,4 +24,13 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  id         INTEGER PRIMARY KEY DEFAULT nextval('meal_seq'),
+  user_id INTEGER NOT NULL ,
+  date_time   TIMESTAMP,
+  description VARCHAR NOT NULL,
+  calories INTEGER NOT NULL
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -11,3 +12,16 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+  (100001, '2017-07-30 10:00:00', 'Завтрак', 300),
+  (100001, '2017-07-30 13:00:00', 'Обед', 700),
+  (100001, '2017-07-30 20:00:00', 'Ужин', 1000),
+  (100000, '2017-07-31 10:00:00', 'Завтрак', 500),
+  (100000, '2017-07-31 13:00:00', 'Обед', 1000),
+  (100000, '2017-07-31 20:00:00', 'Ужин', 510),
+  (100000, '2017-07-01 10:00:00', 'Завтрак', 700),
+  (100000, '2017-07-01 14:00:00', 'Обед', 1000),
+  (100000, '2017-07-01 21:00:00', 'Ужин', 700);
