1ace887942351cbf2cfb21c76db7091ce3e69aac
Andrey <fyling@i.ua>
2017-08-03 14:05:00 +0300

    HW03

--- src/test/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
+++ src/test/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
@@ -18,7 +18,10 @@ import java.util.Collection;
 import static ru.javawebinar.topjava.UserTestData.ADMIN;
 import static ru.javawebinar.topjava.UserTestData.USER;
 
-@ContextConfiguration("classpath:spring/spring-app.xml")
+@ContextConfiguration({
+        "classpath:spring/spring-db.xml",
+        "classpath:spring/spring-app.xml"
+})
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
 
@@ -47,4 +50,11 @@ public class InMemoryAdminRestControllerSpringTest {
     public void testDeleteNotFound() throws Exception {
         controller.delete(10);
     }
+
+    /*
+    @Test
+    public void testSave(User user) {
+        controller.create(user);
+    }
+    */
 }
